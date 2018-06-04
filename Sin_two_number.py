7312ec3fe598c025b3356ec8b832ab1e137aef62
Himanshu Choudhary <hc9599@gmai.com>
2018-06-04 17:52:25 +0530

    gitgt

---
+++ Sin_two_number.py
@@ -0,0 +1,5 @@
+import math as m 
+
+input_1, input_2 = int(input("Enter first number: ")), int(input("\nEnter second number : "))
+
+print("sin of both numbers are: ", m.sin(input_1), m.sin(input_2))
